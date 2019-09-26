c107edb8618a4310da8168548f47c1365bb382a7
Haur Kang <haur.kang@gmail.com>
2019-09-26 23:08:23 +0100

    add more cy commands to test

--- cypress/integration/examples/test1.spec.js
+++ cypress/integration/examples/test1.spec.js
@@ -1,8 +1,10 @@
+/// <reference types="Cypress" />
+
 describe ('Writing cypress test', () => {
 
     it('testing testing', () => {
         cy.visit('https://www.ef.co.uk/');
-
+        cy.get('#omnidiv-tile-misc-st-loc > .tile-mosaic__item > .panel > .panel__default > .panel__title').click()
     })
 
 })
