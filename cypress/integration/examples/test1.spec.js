f56b924acdc87f1820f62f65883402fdfab2555c
Haur Kang <haur.kang@gmail.com>
2019-09-28 23:51:23 +0100

    select one of the content from the menu

--- cypress/integration/examples/test1.spec.js
+++ cypress/integration/examples/test1.spec.js
@@ -7,7 +7,11 @@ describe ('Writing cypress test', () => {
         cy.get('#omnidiv-tile-misc-st-loc > .tile-mosaic__item > .panel > .panel__default > .panel__title').click();
         cy.get('.ef-header__menu-toggle > .ef-icon').click();
         cy.get('.item-content__wrapper').should('have.length', 5);
-        // cy.get('.topbar-menu-toggle ef-icon').click();
+        cy.get('#omnidiv-product-site-menu-mobile > .ef-nav > .ef-nav__menu > :nth-child(2) > :nth-child(1)').click();
+        cy.get('.-open.-with-sub-menu > .ef-nav__sub-menu > :nth-child(2) > .ef-nav__link').click();
+        // cy.get('.item-content__wrapper').find
+
+        
     })
 
 })
