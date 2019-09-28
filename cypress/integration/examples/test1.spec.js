33d18e0c0e6f8063ee00861898af5d83f82f179b
Haur Kang <haur.kang@gmail.com>
2019-09-28 23:44:34 +0100

    check if header has 5 content wrapper

--- cypress/integration/examples/test1.spec.js
+++ cypress/integration/examples/test1.spec.js
@@ -4,7 +4,10 @@ describe ('Writing cypress test', () => {
 
     it('testing testing', () => {
         cy.visit('https://www.ef.co.uk/');
-        cy.get('#omnidiv-tile-misc-st-loc > .tile-mosaic__item > .panel > .panel__default > .panel__title').click()
+        cy.get('#omnidiv-tile-misc-st-loc > .tile-mosaic__item > .panel > .panel__default > .panel__title').click();
+        cy.get('.ef-header__menu-toggle > .ef-icon').click();
+        cy.get('.item-content__wrapper').should('have.length', 5);
+        // cy.get('.topbar-menu-toggle ef-icon').click();
     })
 
 })
