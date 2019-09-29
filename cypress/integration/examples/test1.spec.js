eb887127a7b1ea9622c9918c40c15802a15a5201
Haur Kang <haur.kang@gmail.com>
2019-09-29 22:10:35 +0100

    add assertion , location

--- cypress/integration/examples/test1.spec.js
+++ cypress/integration/examples/test1.spec.js
@@ -5,6 +5,8 @@ describe ('Writing cypress test', () => {
     it('testing testing', () => {
         cy.visit('https://www.ef.co.uk/');
         cy.get('#omnidiv-tile-misc-st-loc > .tile-mosaic__item > .panel > .panel__default > .panel__title').click();
+
+        cy.location('pathname').should('eq', '/corporate/');
         cy.get('.ef-header__menu-toggle > .ef-icon').click();
         cy.get('.item-content__wrapper').should('have.length', 5);
         cy.get('#omnidiv-product-site-menu-mobile > .ef-nav > .ef-nav__menu > :nth-child(2) > :nth-child(1)').click();
