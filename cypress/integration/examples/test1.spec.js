e1ab2d87fa98b1f2228ccfc7d6ef9973c540d9b4
Haur Kang <haur.kang@gmail.com>
2019-09-29 17:56:53 +0100

    use .each method for looping an array of items

--- cypress/integration/examples/test1.spec.js
+++ cypress/integration/examples/test1.spec.js
@@ -8,6 +8,12 @@ describe ('Writing cypress test', () => {
         cy.get('.ef-header__menu-toggle > .ef-icon').click();
         cy.get('.item-content__wrapper').should('have.length', 5);
         cy.get('#omnidiv-product-site-menu-mobile > .ef-nav > .ef-nav__menu > :nth-child(2) > :nth-child(1)').click();
+        // cy.get('.ef-row').find('.ef-col').each(($e1, index, $list) => {
+        //     const text = $e1.find('h3.block-text__title').text()
+        //     if (text.includes('Online courses')) {
+        //         $e1.find('a').click()
+        //     }
+        // })
         cy.get('.-open.-with-sub-menu > .ef-nav__sub-menu > :nth-child(2) > .ef-nav__link').click();
         cy.get('.fat-footer__wrapper').find('.fat-footer__button').click();
         cy.get('#FirstName').type('I am typing the first name');
